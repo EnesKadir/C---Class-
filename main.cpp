4d2f478f5b309b6e53c876d41761af2c69d8124d
Enes kadir  Dönmez <>
2023-04-04 01:20:18 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,39 @@
+//
+//  main.cpp
+//  C++(Class)
+//
+//  Created by Enes kadir  Dönmez on 15.10.2020.
+//
+
+#include <stdio.h>
+
+int main()
+{
+
+    printf("Enter any character");
+    scanf("%d",&chr)
+    
+    if (chr>="a"&& chr<="z")||(chr>="A" && chr<="Z"){
+        printf("Character is an Alphabe");
+    }
+    else if {
+        printf("Character is not Alphabe");
+    }
+        
+    return 0;
+    
+}
+
+
+
+
+    
+    
+    
+    
+    
+
+
+
+
+
---
+++ odev.hpp
@@ -0,0 +1,13 @@
+//
+//  odev.hpp
+//  C++(Class)
+//
+//  Created by Enes kadir  Dönmez on 22.10.2020.
+//
+
+#ifndef odev_hpp
+#define odev_hpp
+
+#include <stdio.h>
+
+#endif /* odev_hpp */
